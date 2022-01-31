5c3fee3f237d265d6e507a92e0218b02f0aa49b8
AkolenDvdnk <ak0lenfs@gmail.com>
2022-01-31 11:54:10 +0200

    Player jump and GroundCheck

---
+++ Unit 3 - Sound and Effects/Assets/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float jumpForce = 10f;
+    public float groundCheckRadius;
+
+    public LayerMask whatIsGround;
+    public Transform groundCheck;
+
+    private bool isGrounded;
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        GetInput();
+        CheckGround();
+    }
+    private void GetInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+    private void CheckGround()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+}
