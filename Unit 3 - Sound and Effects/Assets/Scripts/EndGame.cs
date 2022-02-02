d957df2997d53c3e19ab7dcd90e8067e86e1cf65
AkolenDvdnk <ak0lenfs@gmail.com>
2022-02-02 14:10:48 +0200

    Beta build

---
+++ Unit 3 - Sound and Effects/Assets/Scripts/EndGame.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+    public GameObject panel;
+
+    private void Update()
+    {
+        if (PlayerController.instance.gameOver)
+        {
+            panel.SetActive(true);
+        }
+
+        CheckInput();
+    }
+    public void CheckInput()
+    {
+        if (PlayerController.instance.gameOver)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+            if (Input.GetKeyDown(KeyCode.Escape))
+                Application.Quit();
+    }
+}
